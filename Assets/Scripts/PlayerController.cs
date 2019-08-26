78d9d359cbc584f7a59a7c72a88e822a66104902
Markus Mayer <widemeadows@gmail.com>
2019-08-27 01:27:05 +0200

    Add animation event to trigger script function
    This is adding an event withing the jump animation that is tied
    to the StopJump() method. This method un-sets the boolean flag
    that initiated the running-to-jump transition.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Jump") > 0)
+        {
+            _anim.SetBool(IsJumping, true);
+        }
+    }
+
+    private void StopJump()
+    {
+        _anim.SetBool(IsJumping, false);
+    }
+}
