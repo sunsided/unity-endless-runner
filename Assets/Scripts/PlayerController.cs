861f71bf72af9be282c2a937f1c1ec74b4731a64
Markus Mayer <widemeadows@gmail.com>
2019-08-27 01:43:16 +0200

    Add magic casting animation
    This is using an additional animation layer with a .8 additive
    blend. Due to the legacy character model we apparently can't
    use avatar masks, whatever that means. :)
    
    Anyway, casting a is now tied to Fire1 input and is stopped by
    calling into a script method within an animation event.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int IsMagic = Animator.StringToHash("isMagic");
     private Animator _anim;
 
     private void Start()
@@ -16,10 +18,22 @@ public class PlayerController : MonoBehaviour
         {
             _anim.SetBool(IsJumping, true);
         }
+
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            _anim.SetBool(IsMagic, true);
+        }
     }
 
+    [UsedImplicitly]
     private void StopJump()
     {
         _anim.SetBool(IsJumping, false);
+    }
+
+    [UsedImplicitly]
+    private void StopMagic()
+    {
+        _anim.SetBool(IsMagic, false);
     }
 }
