7d42473ea5a7ed6002c9946f2ebd639e71306aca
Markus Mayer <widemeadows@gmail.com>
2019-08-27 02:29:52 +0200

    Using a dummy traveller object to navigate world

--- Assets/Project/Scripts/GenerateWorld.cs
+++ Assets/Project/Scripts/GenerateWorld.cs
@@ -3,24 +3,38 @@
 public class GenerateWorld : MonoBehaviour
 {
     public GameObject[] platformPrefabs;
+    private GameObject _dummyTraveller;
 
     void Start()
     {
-        var pos = Vector3.zero;
+        _dummyTraveller = new GameObject("dummy");
+
         for (var i = 0; i < 20; ++i)
         {
             var platformNumber = Random.Range(0, platformPrefabs.Length);
-            var go = Instantiate(platformPrefabs[platformNumber], pos, Quaternion.identity);
+            var go = Instantiate(platformPrefabs[platformNumber],
+                _dummyTraveller.transform.position,
+                _dummyTraveller.transform.rotation);
 
-            if (go.CompareTag("stairsUp")) pos.y += 5;
+            if (go.CompareTag("stairsUp"))
+            {
+                _dummyTraveller.transform.Translate(0, 5, 0);
+            }
             else if (go.CompareTag("stairsDown"))
             {
-                pos.y -= 5;
-                go.transform.position = pos;
+                _dummyTraveller.transform.Translate(0, -5, 0);
+                go.transform.position = _dummyTraveller.transform.position;
                 go.transform.Rotate(new Vector3(0, 180, 0));
             }
+            else if (go.CompareTag("platformTSection"))
+            {
+                _dummyTraveller.transform.Rotate(new Vector3(0, 90, 0));
 
-            pos.z -= 10;
+                // T-sections are a bit longer than other platforms, so we need an extra translation here.
+                _dummyTraveller.transform.Translate(Vector3.forward * -10);
+            }
+
+            _dummyTraveller.transform.Translate(Vector3.forward * -10);
         }
     }
 }
