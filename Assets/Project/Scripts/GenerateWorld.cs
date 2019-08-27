43ef969dac80e8aee0d5db4fd6a65530c328e448
Markus Mayer <widemeadows@gmail.com>
2019-08-27 02:04:49 +0200

    Add platform models and trivial generator script

---
+++ Assets/Project/Scripts/GenerateWorld.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    public GameObject[] platformPrefabs;
+
+    void Start()
+    {
+        var pos = Vector3.zero;
+        for (var i = 0; i < 20; ++i)
+        {
+            var platformNumber = Random.Range(0, platformPrefabs.Length);
+            Instantiate(platformPrefabs[platformNumber], pos, Quaternion.identity);
+            pos.z += 10;
+        }
+    }
+}
