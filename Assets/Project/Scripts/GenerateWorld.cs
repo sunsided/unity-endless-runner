7366205e8f65d1ab444dc240a77823fe65118518
Markus Mayer <widemeadows@gmail.com>
2019-08-28 22:38:39 +0200

    Add spawning of new stairs

--- Assets/Project/Scripts/GenerateWorld.cs
+++ Assets/Project/Scripts/GenerateWorld.cs
@@ -17,11 +17,25 @@ namespace Project.Scripts
             {
                 DummyTraveller.transform.position = LastPlatform.transform.position +
                                                     player.transform.forward * 10;
+
+                if (LastPlatform.CompareTag("stairsUp"))
+                {
+                    DummyTraveller.transform.Translate(0, 5, 0);
+                }
             }
 
             LastPlatform = p;
+
             p.transform.position = DummyTraveller.transform.position;
             p.transform.rotation = DummyTraveller.transform.rotation;
+
+            if (p.CompareTag("stairsDown"))
+            {
+                DummyTraveller.transform.Translate(0, -5, 0);
+                p.transform.Rotate(0, 180, 0);
+                p.transform.position = DummyTraveller.transform.position;
+            }
+
             p.SetActive(true);
         }
 
