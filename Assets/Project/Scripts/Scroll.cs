6e721d588c75792651ba68c4de08cabdddeb3aad
Markus Mayer <widemeadows@gmail.com>
2019-08-28 21:56:44 +0200

    Shift world up or down for stairs

--- Assets/Project/Scripts/Scroll.cs
+++ Assets/Project/Scripts/Scroll.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Project.Scripts;
+﻿using Project.Scripts;
 using UnityEngine;
 
 public class Scroll : MonoBehaviour
@@ -15,5 +14,21 @@ public class Scroll : MonoBehaviour
     {
         const float speed = -0.1f;
         transform.position += _player.transform.forward * speed;
+
+        var currentPlatform = PlayerController.CurrentPlatform;
+        if (currentPlatform == null) return;
+
+        const float stairSlope = 0.06f;
+        if (currentPlatform.CompareTag("stairsUp"))
+        {
+            // Stairs are at a 60 degree angle.
+            // For every one step forward, move the "world" 6 steps down.
+            transform.Translate(0, -stairSlope, 0);
+        }
+        else if (currentPlatform.CompareTag("stairsDown"))
+        {
+            // Same logic as above, just in reverse.
+            transform.Translate(0, stairSlope, 0);
+        }
     }
 }
