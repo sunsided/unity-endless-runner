a5b6031227e5f382cfd6fde3bd57dba03b5c8620
Markus Mayer <widemeadows@gmail.com>
2019-08-28 21:56:25 +0200

    Fix inputs to be more natural (AD move, QE rotate)

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ namespace Project.Scripts
     public class PlayerController : MonoBehaviour
     {
         public static GameObject Player;
+        public static GameObject CurrentPlatform;
 
         private static readonly int IsJumping = Animator.StringToHash("isJumping");
         private static readonly int IsMagic = Animator.StringToHash("isMagic");
@@ -22,13 +23,18 @@ namespace Project.Scripts
             _anim = GetComponent<Animator>();
         }
 
+        private void OnCollisionEnter([NotNull] Collision other)
+        {
+            CurrentPlatform = other.gameObject;
+        }
+
         private void Update()
         {
-            var horzDown = Input.GetButtonDown("Horizontal");
-            var horz = Input.GetAxisRaw("Horizontal") * (horzDown ? 1 : 0);
+            var rotateDown = Input.GetButtonDown("Rotate");
+            var rotate = Input.GetAxisRaw("Rotate") * (rotateDown ? 1 : 0);
 
-            var sideDown = Input.GetButtonDown("Side Shift");
-            var side = Input.GetAxisRaw("Side Shift") * (sideDown ? 1 : 0);
+            var shiftDown = Input.GetButtonDown("Horizontal");
+            var shift = Input.GetAxisRaw("Horizontal") * (shiftDown ? 1 : 0);
 
             if (Input.GetAxisRaw("Jump") > 0)
             {
@@ -38,19 +44,19 @@ namespace Project.Scripts
             {
                 _anim.SetBool(IsMagic, true);
             }
-            else if (horz > 0)
+            else if (rotate > 0)
             {
                 transform.Rotate(Vector3.up * 90);
             }
-            else if (horz < 0)
+            else if (rotate < 0)
             {
                 transform.Rotate(Vector3.up * -90);
             }
-            else if (side > 0)
+            else if (shift > 0)
             {
                 transform.Translate(0.1f, 0, 0);
             }
-            else if (side < 0)
+            else if (shift < 0)
             {
                 transform.Translate(-0.1f, 0, 0);
             }
