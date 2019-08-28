edfd712555af9389bba09e9ff199c63778233b1e
Markus Mayer <widemeadows@gmail.com>
2019-08-28 21:41:54 +0200

    Fix inputs to use actual "Side" axis

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -27,8 +27,8 @@ namespace Project.Scripts
             var horzDown = Input.GetButtonDown("Horizontal");
             var horz = Input.GetAxisRaw("Horizontal") * (horzDown ? 1 : 0);
 
-            var vertDown = Input.GetButtonDown("Vertical");
-            var vert = Input.GetAxisRaw("Vertical") * (vertDown ? 1 : 0);
+            var sideDown = Input.GetButtonDown("Side Shift");
+            var side = Input.GetAxisRaw("Side Shift") * (sideDown ? 1 : 0);
 
             if (Input.GetAxisRaw("Jump") > 0)
             {
@@ -46,11 +46,11 @@ namespace Project.Scripts
             {
                 transform.Rotate(Vector3.up * -90);
             }
-            else if (vert > 0)
+            else if (side > 0)
             {
                 transform.Translate(0.1f, 0, 0);
             }
-            else if (vert < 0)
+            else if (side < 0)
             {
                 transform.Translate(-0.1f, 0, 0);
             }
