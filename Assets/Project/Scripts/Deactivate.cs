f9a24889b83bfd4a82161b4acb48e04e2d5e3c7f
Markus Mayer <widemeadows@gmail.com>
2019-08-28 23:03:45 +0200

    Fix double despawn scheduling issue
    This commit fixes a bug where double bounces onto the same segment
    scheduled despawning of the same object twice.
    
    This causing a situation where an item was already put into the pool
    upon the first despawn, but was scheduled for despawning a second time
    before even moving back to the pool.
    
    This happened in certain situations around stairs, where the physics
    sent the player off jumping.

--- Assets/Project/Scripts/Deactivate.cs
+++ Assets/Project/Scripts/Deactivate.cs
@@ -4,20 +4,24 @@ namespace Project.Scripts
 {
     public class Deactivate : MonoBehaviour
     {
+        private bool _deactivationScheduled;
+
         private void OnCollisionExit(Collision other)
         {
-            if (other.gameObject.CompareTag("Player"))
+            if (other.gameObject.CompareTag("Player") && !_deactivationScheduled)
             {
                 // Make sure that the element is behind the camera enough
                 // to not see it de-spawning.
                 // TODO: The T-section is rather long - we need to ensure we actually left it before de-spawning.
-                Invoke(nameof(SetInactive), 3.0f);
+                _deactivationScheduled = true;
+                Invoke(nameof(SetInactive), 4.0f);
             }
         }
 
         private void SetInactive()
         {
             gameObject.SetActive(false);
+            _deactivationScheduled = false;
         }
     }
 }
