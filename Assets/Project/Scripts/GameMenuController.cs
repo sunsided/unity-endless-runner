3209be769eba93edb9f1b7bf3460527f3d65b099
Markus Mayer <widemeadows@gmail.com>
2019-08-29 02:41:02 +0200

    Add game starting, stopping and quitting menus

---
+++ Assets/Project/Scripts/GameMenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Scripts
+{
+    public class GameMenuController : MonoBehaviour
+    {
+        public void LoadMenuScene()
+        {
+            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Scripts
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        public void LoadGameScene()
+        {
+            SceneManager.LoadScene("ScrollingWorld", LoadSceneMode.Single);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
