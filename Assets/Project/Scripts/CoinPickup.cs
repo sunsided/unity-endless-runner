3c544ab749e5841b0aa60b34e5ef678cef45705c
Markus Mayer <widemeadows@gmail.com>
2019-08-30 02:03:00 +0200

    Fix permanent coin destruction

--- Assets/Project/Scripts/CoinPickup.cs
+++ Assets/Project/Scripts/CoinPickup.cs
@@ -1,4 +1,5 @@
-﻿    using UnityEngine;
+﻿    using System;
+    using UnityEngine;
 
 namespace Project.Scripts
 {
@@ -6,13 +7,34 @@ namespace Project.Scripts
     {
         public int value = 10;
 
+        private MeshRenderer[] _renderers;
+
+        private void Start()
+        {
+            _renderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            if (_renderers == null) return;
+            for (var i = 0; i < _renderers.Length; ++i)
+            {
+                _renderers[i].enabled = true;
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.CompareTag("Player")) return;
             Debug.Log("Coin picked up.");
 
             GameData.Singleton.AddScore(value);
-            Destroy(gameObject, 0.5f);
+
+            // Instead of destroying the coin, just hide it.
+            for (var i = 0; i < _renderers.Length; ++i)
+            {
+                _renderers[i].enabled = false;
+            }
         }
     }
 }
