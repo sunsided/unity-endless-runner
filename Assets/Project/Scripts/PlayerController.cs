ecca70e01d0149ef2b03ed6ceee6cf5de4bc0972
Markus Mayer <m.mayer@nyris.io>
2019-08-31 18:13:43 +0200

    Add spell sounds

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -183,6 +183,7 @@ namespace Project.Scripts
         [UsedImplicitly]
         private void CastMagic()
         {
+            GameData.Singleton.SoundCastMagic.Play();
             magic.transform.position = magicStartPosition.position;
             magic.SetActive(true);
             _magicRb.AddForce(transform.forward * 20, ForceMode.Impulse);
