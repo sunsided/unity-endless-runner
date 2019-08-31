757bbda2dd3120b33b4afa60e130b50eec059a16
Markus Mayer <m.mayer@nyris.io>
2019-08-31 16:49:26 +0200

    Add music volume slider

---
+++ Assets/Project/Scripts/UpdateMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts
+{
+    public class UpdateMusic : MonoBehaviour
+    {
+        private readonly List<AudioSource> _music = new List<AudioSource>();
+
+        private void Start()
+        {
+            var allAS = GameObject.FindWithTag("GameData").GetComponentsInChildren<AudioSource>();
+            Debug.Assert(allAS.Length > 0, "allAS.Length > 0");
+            _music.Add(allAS[0]);
+        }
+
+        public void UpdateMusicVolume([NotNull] Slider slider) => UpdateMusicVolume(slider.value);
+
+        public void UpdateMusicVolume(float volume)
+        {
+            foreach (var source in _music)
+            {
+                source.volume = volume;
+            }
+        }
+    }
+}
