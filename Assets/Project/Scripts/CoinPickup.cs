c7bf0cc9fa8f20b2525dbd7705e46bced723aae3
Markus Mayer <m.mayer@nyris.io>
2019-08-31 18:44:44 +0200

    Add floating score text on coin pickup

--- Assets/Project/Scripts/CoinPickup.cs
+++ Assets/Project/Scripts/CoinPickup.cs
@@ -1,21 +1,38 @@
 ﻿using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Project.Scripts
 {
     public class CoinPickup : MonoBehaviour
     {
-        public int value = 10;
+        [Range(0, 100)]
+        public float highValueProbability = 10;
 
+        public int value = 10;
+        public int highValue = 50;
+
+        public Material regularMaterial;
+        public Material highValueMaterial;
+        public GameObject scorePrefab;
+
+        private bool _isBigCoin;
+        private int _score;
+        private Canvas _canvas;
         private MeshRenderer[] _renderers;
 
         private void Start()
         {
+            _canvas = FindObjectOfType<Canvas>();
             _renderers = GetComponentsInChildren<MeshRenderer>();
+
+            RandomizeScore();
         }
 
         private void OnEnable()
         {
+            RandomizeScore();
+
             if (_renderers == null) return;
             for (var i = 0; i < _renderers.Length; ++i)
             {
@@ -26,14 +43,32 @@ namespace Project.Scripts
         private void OnTriggerEnter([NotNull] Collider other)
         {
             if (!other.gameObject.CompareTag("Player")) return;
-            GameData.Singleton.SoundPickup.Play();
 
-            GameData.Singleton.AddScore(value);
+            GameData.Singleton.SoundPickup.Play();
+            GameData.Singleton.AddScore(_score);
+
+            // Show the score text float.
+            var scoreText = Instantiate(scorePrefab, _canvas.transform);
+            scoreText.GetComponent<Text>().text = _score.ToString();
 
             // Instead of destroying the coin, just hide it.
             for (var i = 0; i < _renderers.Length; ++i)
             {
                 _renderers[i].enabled = false;
+            }
+        }
+
+        private void RandomizeScore()
+        {
+            _isBigCoin = Random.Range(1, 101) <= highValueProbability;
+            _score = _isBigCoin ? highValue : value;
+            var material = _isBigCoin ? highValueMaterial : regularMaterial;
+
+            if (_renderers == null) return;
+            for (var i = 0; i < _renderers.Length; ++i)
+            {
+                _renderers[i].enabled = true;
+                _renderers[i].material = material;
             }
         }
     }
---
+++ Assets/Project/Scripts/FloatUpText.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts
+{
+    public class FloatUpText : MonoBehaviour
+    {
+        public float floatSpeed = 100;
+        public float fadeSpeed = 5f;
+
+        private Text _text;
+        private float _alpha;
+
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+            _text.color = Color.white;
+        }
+
+        private void OnEnable()
+        {
+            _alpha = 1;
+        }
+
+        private void Update()
+        {
+            // TODO: We may want to use the time delta in here ...
+            transform.Translate(0, floatSpeed * Time.deltaTime, 0);
+
+            _alpha -= fadeSpeed * Time.deltaTime;
+            if (_alpha <= 0f)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            var color = _text.color;
+            _text.color = new Color(color.r, color.g, color.b, _alpha);
+        }
+    }
+}
